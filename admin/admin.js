16f63a9bd07734c75bda052f1f3dcc1fdbfd34af
tomothyc <thomas.cotter44@gmail.com>
2021-08-27 12:47:01 +0800

    Started admin page, basic form dependencies

---
+++ admin/admin.js
@@ -0,0 +1,23 @@
+window.onchange = init;
+window.onload = init;
+function init() {
+  var exercises = document.getElementsByClassName("exercise");
+  for (var i = 0; i < exercises.length; i++) {
+    exercises[i].style.display = "none";
+  }
+
+  var option = document.getElementById("step").value;
+  console.log(option);
+  if (option == "letters") {
+    document.getElementById("lettersexercise").style.display = "block";
+  }
+  if (option == "syllables") {
+    document.getElementById("syllablesexercise").style.display = "block";
+  }
+  if (option == "words") {
+    document.getElementById("wordsexercise").style.display = "block";
+  }
+  if (option == "dictation") {
+    document.getElementById("dictationexercise").style.display = "block";
+  }
+}
